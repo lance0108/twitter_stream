fefb71b2d624d7bc417dce630427ba95644562e2
Lance <lance0108@gmail.com>
2020-03-21 16:28:25 -0500

    Updated SQL

--- create_tables.sql
+++ create_tables.sql
@@ -6,6 +6,8 @@ create table if not exists twitter.corona (
 );
 
 CREATE INDEX ON twitter.corona (id);
+CREATE INDEX ON twitter.corona (created_at);
+CREATE INDEX ON twitter.corona (lang);
 
 create table if not exists twitter.nyc (
 	id bigint primary key,
@@ -15,3 +17,16 @@ create table if not exists twitter.nyc (
 );
 
 CREATE INDEX ON twitter.nyc (id);
+CREATE INDEX ON twitter.nyc (created_at);
+CREATE INDEX ON twitter.nyc (lang);
+
+create table if not exists twitter.chicago (
+	id bigint primary key,
+	created_at timestamp,
+	lang varchar(5),
+	raw_json json
+);
+
+CREATE INDEX ON twitter.chicago (id);
+CREATE INDEX ON twitter.chicago (created_at);
+CREATE INDEX ON twitter.chicago (lang);
